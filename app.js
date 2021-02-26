cb71ebd6e3561c9fd8acd61a548441bc6433f0ae
abidtkg <abidtkg@gmail.com>
2021-02-26 14:52:06 +0600

    Swagger Def Initiated

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('./configs/swagger.config');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// ALLOW CROSS ORIGIN ACCESS
+app.use(cors());
+
+// JSON ONLY DEFINED
+app.use(express.json());
+
+// PARSE APPLICATION/JSON
+app.use(bodyParser.json());
 
 // Import Router
 const homeRoutes = require('./routes/home.routes');
@@ -8,6 +21,7 @@ const errorRoutes = require('./routes/error.routes');
 
 // Implement Routes
 app.use('/', homeRoutes);
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('**', errorRoutes);
 
 // Server Port To Listen
---
+++ configs/swagger.config.js
@@ -0,0 +1,35 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+// SWAGGER CONFIGARATION
+const swaggerOptions = {
+    swaggerDefinition: {
+      openapi: '3.0.0',
+        info: {
+            title: "Todo API",
+            description: "Todo Application API Specification",
+            termsOfService: "",
+            version: "1.0.0"
+        },
+        contact: {
+            name: "API Support",
+            url: "#",
+            email: "contact@abid.app"
+        },
+        servers: [
+            {
+              "url": "http://locahost:3000",
+              "description": "Development server"
+            }
+          ],
+        securityDefinitions: {
+          APIKeyHeader: {
+            type: 'apiKey',
+            in: 'header',
+            name: 'token'
+          },
+        }
+    },
+
+    apis: ['routes/*.js']
+}
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+module.exports = swaggerDocs;
